629f9f6ce3500bbd247da1049a055160ee034a5a
Silvia20-star <Silvitahg@comunidad.unam.mx>
2020-06-03 02:27:18 -0500

    FEAT: se agregó Actividad2.php.

---
+++ Actividad2.php
@@ -0,0 +1,17 @@
+<?php
+  echo "<!DOCTYPE html>
+  <html>
+  <head>
+      <title></title>
+  </head>
+  <body style='background-color:#FF44FF'>
+  <h2>Actividad 2</h2>
+  <h3>Silvia Herrera González.</h3>
+  Hasta el momento las dos cosas que más me han gustado y he aprendido del curso son:
+  <ul>
+      <li>Crear páginas con HTML.</li>
+      <li>Crear páginas con PHP.</li>
+  </ul>
+  </body>
+  </html>"
+?>
